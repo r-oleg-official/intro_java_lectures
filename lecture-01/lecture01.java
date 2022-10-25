b221c5af6b97cd281b92375a5c52e1feaeede94f
Oleg Razumov <r.oleg.official@gmail.com>
2022-10-25 17:04:47 +0300

    Edit lecture-01/lecture01.java lecture-01/lecture-01.md.

--- lecture-01/lecture01.java
+++ lecture-01/lecture01.java
@@ -18,23 +18,40 @@ public class lecture01 {
     public static void main(String[] args) {
         typeInt();
         System.out.println();
+
         typeDouble();
         System.out.println();
+        
         typeChar();
         System.out.println();
+        
         convIntDouble();
+        
         typeBoolean();
         System.out.println();
+        
         typeString();
         System.out.println();
+        
         typeImplicit();
+        System.out.println();
+        
+        classesWrapper();
+        System.out.println();
+        
+        operationsJava();
+        System.out.println();
+
+        toBitOperations();
     }
     
     public static void typeInt() {
         short age = 10;
         int salary = 123456;
-        System.out.println(age); //10
+        int i = 23_123_34;          //2312334
+        System.out.println(age);    //10
         System.out.println(salary); //123456
+        System.out.println(i);
     }
     
     public static void typeDouble() {
@@ -95,6 +112,119 @@ public class lecture01 {
     }
 
     static String getType(Object o) {
-        return o.getClass().getSimpleName();
+        return o.getClass().getSimpleName(); //  метод получения тип данных
+    }
+
+    public static void classesWrapper() {
+        // Классы-обертки
+        int a = 123;
+        System.out.println(Integer.MAX_VALUE);
+        char b = '2';
+        System.out.println(Character.isDigit(b)); // true
+        String s = "qwer";
+        System.out.println(s.length());     // 4
+        System.out.println(s.charAt(1));    // w
+    }
+
+    public static void operationsJava() {
+        int a = 123;
+        a++;
+        System.out.println(a);  // 124
+
+        a = 123;
+        System.out.println(a++);    // 123
+        System.out.println(a);      // 124
+
+        a = 125;
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println();
+
+        // недобросовестные интервьюеры спрашивают
+        System.out.println("Недобросовестные интервьюеры");
+        a = 123;
+        a = a-- - --a;
+        System.out.println(a); // 2
+        a = --a - a--;
+        System.out.println(a);  // 0
+        a = --a-a--;
+        System.out.println(a);  // 0
+        System.out.println();
+        
+        System.out.println("Инкремент в цикле 'for'");
+        for(int i = 0; i < 3; i++) {
+            System.out.println(i);      // 0 1 2
+        }
+        System.out.println();
+
+        for(int i = 0; i < 3; ++i) {
+            System.out.println(i);      // 0 1 2
+        }
+        System.out.println();
+
+        for(int i = 0; i < 3;) {
+            System.out.println(i++);    // 0 1 2
+        }
+        System.out.println();
+
+        // Операции сравнения
+        System.out.println("Операции сравнения");
+        boolean f = 123 > 234;
+        System.out.println(f);  // false
+        f = 123 <= 234;
+        System.out.println(f);  // true
+        f = 123 != 234;
+        System.out.println(f);  // true
+    }
+
+    public static void toBitOperations() {
+        // побитовые операции
+        int a = 8;      // 1000 - в битах
+        // a = a << 1;  // 10000
+        // System.out.println(a);   // 16
+        System.out.println(a << 1); // 16
+        
+        a = 18;      // 10010 - в битах
+        // a = a >> 1;  // 1001
+        // System.out.println(a);   // 9
+        System.out.println(a >> 1); // 9
+        
+        int b = 5;
+        int c = 2;
+        System.out.println(b | c);  // 7
+        // 101 - 5
+        // 010 - 2
+        // 111 - 7
+
+        System.out.println(b & c);  // 0
+        // 101 - 5
+        // 010 - 2
+        // 000 - 0
+        
+        System.out.println(b ^ c);  // 0
+        // 101 - 5
+        // 010 - 2
+        // 111 - 7
+
+        boolean d = true;
+        boolean e = true;
+        System.out.println(d & e);  // true
+        System.out.println();
+
+        String s = "qwer"; // длина 4, 0..3
+        boolean i = s.length() >= 5 && s.charAt(4) == '1'; // false && false
+        System.out.println(i); // false
+
+        s = "qwwe1"; // длина 5, 0..4
+        i = s.length() >= 5 && s.charAt(4) == '1'; // true && true
+        System.out.println(i); // true
+        i = s.length() >= 5 & s.charAt(4) == '1'; // true && true
+        System.out.println(i); // true
+
+        s = "qww1"; // длина 4, 0..3
+        i = s.length() >= 5 & s.charAt(4) == '1'; // false && false
+        System.out.println(i); // Exception
+        
+
     }
 }
